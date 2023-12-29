1aed8ac2a02017b3d1b878f35318be5a07662371
김하나 <dd1246gksk@gmail.com>
2023-12-29 23:08:07 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/example/capd/CapDApplication.java
@@ -0,0 +1,13 @@
+package com.example.capd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapDApplication.class, args);
+    }
+
+}
