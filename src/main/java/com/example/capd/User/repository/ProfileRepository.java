20064ac64cb64353f8ffff2d3778f245bacaed34
김하나 <dd1246gksk@gmail.com>
2024-01-14 18:08:31 +0900

    [Feat] stackRecommendUsers 추가

--- src/main/java/com/example/capd/User/repository/ProfileRepository.java
+++ src/main/java/com/example/capd/User/repository/ProfileRepository.java
@@ -2,6 +2,7 @@ package com.example.capd.User.repository;
 
 import com.example.capd.User.domain.Career;
 import com.example.capd.User.domain.Profile;
+import com.example.capd.User.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,8 +16,14 @@ public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
     Optional<Profile> findByUserId(Long userId);
 
-    List<Profile> findProfilesByStackList(List<String> stackList);
-
     @Query("SELECT p FROM Profile p LEFT JOIN FETCH p.careers c WHERE p.user.userId = :userId")
     Optional<Profile> findProfileAndCareersByUserId(@Param("userId") String userId);
+
+
+    @Query("SELECT DISTINCT u FROM User u " +
+            "JOIN u.profile p " +
+            "WHERE EXISTS " +
+            "(SELECT 1 FROM p.stackList s WHERE s IN :stackList)")
+    List<User> findUsersByStackList(@Param("stackList") List<String> stackList);
 }
+
--- src/main/java/com/example/capd/User/repository/UserRepository.java
+++ src/main/java/com/example/capd/User/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.example.capd.User.repository;
 
+import com.example.capd.User.domain.Profile;
 import com.example.capd.User.domain.Review;
 import com.example.capd.User.domain.Team;
 import com.example.capd.User.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +16,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserId(String userId);
     List<Review> findReceivedReviewsByUserId(String userId);
+
+    @Query("SELECT u FROM User u JOIN Participation part ON u.id = part.user.id WHERE part.contest.id = :contestId")
+    List<User> findUsersByContestParticipation(@Param("contestId") Long contestId);
+
 }
